a96fd2cdb6a853b04ee579b1831c09bcbd220cf3
conrellreddick <admin@dev.lawgen.org>
2018-07-15 11:58:52 -0400

    Create the IAppointment Interface and Added a Class AppointmentService to implement the interface.

---
+++ BHOD/Data/IAppointment.cs
@@ -0,0 +1,23 @@
+﻿using BHOD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BHOD.Data
+{
+    interface IAppointment
+    {
+        IEnumerable<Appointment> GetAll();
+        Appointment GetById(int AppointmentId);
+        void Add(Appointment newAppointment);
+        void AppointmentOut(int personalId, int paymentMethodId);
+        void AppointmentIn(int personalId, int paymentMethodId);
+        IEnumerable<AppointmentHistory> GetAppointmentHistory(int id);
+
+        void Reserved(int personalId, int paymentMethodId);
+        string GetCurrentPreBookedCustomerName(int id);
+        DateTime GetCurrentPreBookedSchedule(int id);
+        IEnumerable<PreBookedAppointments> GetCurrentPreBooked(int id);
+    }
+}
---
+++ BHOD/Services/AppointmentService.cs
@@ -0,0 +1,62 @@
+﻿using BHOD.Data;
+using BHOD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BHOD.Services
+{
+    public class AppointmentService : IAppointment
+    {
+        public void Add(Appointment newAppointment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AppointmentIn(int personalId, int paymentMethodId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AppointmentOut(int personalId, int paymentMethodId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Appointment> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<AppointmentHistory> GetAppointmentHistory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Appointment GetById(int AppointmentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PreBookedAppointments> GetCurrentPreBooked(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCurrentPreBookedCustomerName(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetCurrentPreBookedSchedule(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reserved(int personalId, int paymentMethodId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
